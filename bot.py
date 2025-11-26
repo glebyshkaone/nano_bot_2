c4ceb84818000c752a917b0abc3e9834fe50dff1
glebyshkaone <56454617+glebyshkaone@users.noreply.github.com>
2025-11-26 16:10:24 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,55 +1,20 @@
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, filters
+from telegram.ext import ApplicationBuilder
 
-# local imports
 from config import TELEGRAM_BOT_TOKEN
 from utils.logger import setup_logger
-
-from handlers.router import (
-    start_command,
-    menu_command,
-    help_command,
-    balance_command,
-    history_command,
-    handle_reply_buttons,
-)
-
-from handlers.admin_handlers import (
-    admin_main,
-    admin_callback,
-    add_tokens_command
-)
-
-from handlers.photo_handlers import handle_photo
-
-from handlers.settings_handlers import settings_callback
+from handlers.router import register_handlers
 
 
-def main():
+def main() -> None:
     logger = setup_logger()
+    logger.info("Starting nano-bot...")
 
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
-    logger.info("Bot started")
+    # Регистрируем все хендлеры в одном месте
+    register_handlers(application)
 
-    # User commands
-    application.add_handler(CommandHandler("start", start_command))
-    application.add_handler(CommandHandler("menu", menu_command))
-    application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("balance", balance_command))
-    application.add_handler(CommandHandler("history", history_command))
-
-    # Admin commands
-    application.add_handler(CommandHandler("admin", admin_main))
-    application.add_handler(CommandHandler("add_tokens", add_tokens_command))
-
-    # Callbacks (admin first)
-    application.add_handler(CallbackQueryHandler(admin_callback, pattern="^admin_"))
-    application.add_handler(CallbackQueryHandler(settings_callback))
-
-    # Messages
-    application.add_handler(MessageHandler(filters.PHOTO, handle_photo))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_reply_buttons))
-
+    logger.info("Application is running (polling)")
     application.run_polling()
 
 
