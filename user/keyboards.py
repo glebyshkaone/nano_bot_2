69a4e1e8751a937b0937ac03ac377c13c1c9e35c
glebyshkaone <56454617+glebyshkaone@users.noreply.github.com>
2025-11-26 17:13:14 +0500

    Create keyboards.py

---
+++ user/keyboards.py
@@ -0,0 +1,8 @@
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+
+def build_reply_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = [
+        [KeyboardButton("🚀 Старт"), KeyboardButton("🎛 Меню")],
+        [KeyboardButton("💰 Баланс"), KeyboardButton("📜 История"), KeyboardButton("ℹ Помощь")],
+    ]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
